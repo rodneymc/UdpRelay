rodneymc/UdpRelay

commit 183b0828c9b6d0c32cfb2946b99c119d4fd6b6da

Rudimentary error handling, and rehash of how we check for new relays

app/src/main/java/com/daftdroid/simpleapps/udprelay/NetworkService.java
@@ -7,6 +7,7 @@ import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
+import android.support.v4.content.LocalBroadcastManager;
 
 import java.io.IOException;
 import java.nio.channels.SelectionKey;
@@ -17,6 +18,15 @@ import java.util.List;
 import java.util.Set;
 
 public class NetworkService extends IntentService {
+
+    public static final String BROADCAST_ACTION =
+            "com.daftdroid.simpleapps.udprelay.NETSERVICE_BROADCAST";
+    public static final String BROADCAST_ERRSTATUS =
+            "com.daftdroid.simpleapps.udprelay.NETSERVICE_ERRSTATUS";
+    public static final String BROADCAST_ERRSTRING =
+            "com.daftdroid.simpleapps.udprelay.NETSERVICE_ERRSTRING";
+    public static final String BROADCAST_RLYNUM =
+            "com.daftdroid.simpleapps.udprelay.NETSERVICE_RLYNUM";
 
 
     /**
@@ -52,8 +62,7 @@ public class NetworkService extends IntentService {
     private static NetworkService singleton;
     private static List<Relay> newRelays; // Static, incase a network service instance isn't created yet
     private static volatile boolean changed;
-
-    private volatile boolean finishing;
+    private boolean finishing;
 
     @Override
     protected void onHandleIntent(Intent intent) {
@@ -105,84 +114,30 @@ public class NetworkService extends IntentService {
         changed = true;
 
         while (true) {
+            while (processRegisteredRelays()) {
+            }
+
+            if (finishing) {
+                /*
+                    If the above call decided we were done, then we are done, there may be a new
+                    one being launched (it's an unlikely race but it could happen) but this instance
+                    is done.
+                 */
+                break;
+            }
+
             try {
-
-                List<Relay> newRelaysCpy = null;
-
-                synchronized (NetworkService.class)
-                {
-                    if (changed) {
-                        // See if any new relays have appeared
-
-
-                        // Add all the new relays into the registeredRelays list, and create
-                        // a new blank list of relays to register, however keep a reference to the
-                        // old one (newRelays) - these are to be initialized outside the syncrhonized block)
-
-                        if (newRelays != null && newRelays.size() > 0)
-                        {
-                            // Once we leave the syncrhonized block, newRelays could be set
-                            // again by another thread.
-
-                            newRelaysCpy = newRelays;
-                            newRelays = null;
-
-                            for (Relay r: newRelaysCpy) {
-                                registeredRelays.add(r);
-                            }
-                        }
-
-                        for (Iterator<Relay> itr = registeredRelays.iterator(); itr.hasNext();) {
-                            Relay r = itr.next();
-
-                            boolean hasError = r.hasError();
-
-                            if (hasError) {
-                                r.suspendRelay();
-
-                                // TODO ui error handling
-                            }
-                            if (hasError || r.stopping()) {
-                                itr.remove();
-                                r.close();
-                            }
-
-                        }
-                        /*
-                            We have processed all the changes to static data.
-                         */
-                        changed = false;
-
-                        // If there are no relays, we can quit
-                        if (registeredRelays.size() == 0) {
-                            finishing = true;
-                            registeredRelays = null;
-                            break;
-                        }
-                    }
-
-                    if (newRelaysCpy != null)
-                    {
-                        for (Relay r: newRelaysCpy)
-                        {
-                            r.initialize(selector);
-                        }
-                    }
-                }
                 selector.select();
                 Set<SelectionKey> keySet = selector.selectedKeys();
 
-                for (SelectionKey key: keySet)
-                {
+                for (SelectionKey key : keySet) {
                     Object attachment;
 
-                    if ((attachment = key.attachment()) instanceof Relay)
-                    {
+                    if ((attachment = key.attachment()) instanceof Relay) {
                         Relay r = (Relay) attachment;
                         r.select(key);
                     }
                 }
-
             } catch (IOException e) {
                 throw new IllegalStateException("IOException here is a TODO", e); // TODO
             }
@@ -192,6 +147,70 @@ public class NetworkService extends IntentService {
         catch (IOException e) {}
     }
 
+    private boolean processRegisteredRelays() {
+        List<Relay> newRelaysCpy = null;
+
+        synchronized(NetworkService.class) {
+            if (changed) {
+                // See if any new relays have appeared
+
+
+                // Add all the new relays into the registeredRelays list, and create
+                // a new blank list of relays to register, however keep a reference to the
+                // old one (newRelays) - these are to be initialized outside the syncrhonized block)
+
+                if (newRelays != null && newRelays.size() > 0) {
+                    // Once we leave the syncrhonized block, newRelays could be set
+                    // again by another thread.
+
+                    newRelaysCpy = newRelays;
+                    newRelays = null;
+
+                    for (Relay r : newRelaysCpy) {
+                        registeredRelays.add(r);
+                    }
+                }
+                changed = false;
+
+        } else {
+                return false; // syncrhonized confirmation there is no more to do
+            }
+        }
+
+        if (newRelaysCpy != null) {
+            for (Relay r: newRelaysCpy) {
+                r.initialize(selector);
+            }
+        }
+
+        for (Iterator<Relay> itr = registeredRelays.iterator(); itr.hasNext();) {
+            Relay r = itr.next();
+
+            boolean hasError = r.hasError();
+
+            if (hasError) {
+                r.suspendRelay();
+                broadcastError(r, Relay.ERROR_HARD, "Error");
+            }
+            if (hasError || r.stopping()) {
+                itr.remove();
+                r.close();
+            }
+        }
+
+        // If there are no relays, we can quit
+
+        synchronized (NetworkService.class) {
+            if (registeredRelays.size() == 0) {
+                singleton = null; // tell async threads
+                finishing = true; // tell the caller
+                registeredRelays = null;
+                return false;
+            }
+        }
+
+        return true;
+    }
     public static synchronized  void uiAddRelay(Context ui, Relay r) {
 
         /*
@@ -223,32 +242,21 @@ public class NetworkService extends IntentService {
         }
     }
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-
-        /*
-            Don't pull the rug while static methods have the lock on newRelays otherwise we have
-            a small window for NPE on code like if (singleton != null && !singleton.finishing)
-         */
-
-        synchronized (NetworkService.class) {
-            if (singleton == null) {
-                /*
-                    If it was null already, then another instance's onDestroy method must have
-                    been called.
-                 */
-                throw new IllegalStateException("Single-instance-of-service logic failure");
-            }
-            singleton = null;
-        }
-    }
-
     public static synchronized List<Relay> getActiveRelays() {
         if (singleton == null) {
             return null;
         } else {
             return new ArrayList<Relay>(singleton.registeredRelays);
         }
+    }
+
+    private void broadcastError(Relay r, int errlevel, String error) {
+
+        Intent localIntent = new Intent(BROADCAST_ACTION)
+                .putExtra(BROADCAST_ERRSTATUS, errlevel)
+                .putExtra(BROADCAST_ERRSTRING, error)
+                .putExtra(BROADCAST_RLYNUM, r.getUniqueId());
+
+        LocalBroadcastManager.getInstance(this).sendBroadcast(localIntent);
     }
 }

app/src/main/java/com/daftdroid/simpleapps/udprelay/MainActivity.java
@@ -20,6 +20,8 @@ This file is part of UdpRelay.
 package com.daftdroid.simpleapps.udprelay;
 
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -39,6 +41,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*
+            Register for receiving status updates to the buttons in the event
+            of network errors.
+         */
+        LocalBroadcastManager.getInstance(this).registerReceiver(
+                new NetServiceBroadcastReceiver(this),
+                new IntentFilter(NetworkService.BROADCAST_ACTION));
 
         // TODO using the demo configs, do something better than this, but if you want to get
         // going you can edit the demo configs to fit your system
@@ -82,6 +92,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         for (RelayButton rb: relays) {
             if (rb.getRelay().equals(r)) {
+                return rb;
+            }
+        }
+
+        return null;
+    }
+
+    public RelayButton getRelayButton(int id) {
+        for (RelayButton rb: relays) {
+            Relay r = rb.getRelay();
+
+            if (r != null && r.getUniqueId() == id) {
                 return rb;
             }
         }

app/src/main/java/com/daftdroid/simpleapps/udprelay/NetServiceBroadcastReceiver.java
@@ -1,0 +1,26 @@
+package com.daftdroid.simpleapps.udprelay;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class NetServiceBroadcastReceiver extends BroadcastReceiver {
+
+    private final MainActivity main;
+
+    public NetServiceBroadcastReceiver(MainActivity main) {
+        this.main = main;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        RelayButton rb = main.getRelayButton(intent.getIntExtra(NetworkService.BROADCAST_RLYNUM, 0));
+        String errString = intent.getStringExtra(NetworkService.BROADCAST_ERRSTRING);
+        int status = intent.getIntExtra(NetworkService.BROADCAST_ERRSTATUS, 0);
+
+        if (rb != null) { // not sure it should ever be null..
+            rb.setError(status, errString);
+        }
+    }
+}

app/src/main/java/com/daftdroid/simpleapps/udprelay/Relay.java
@@ -41,6 +41,13 @@ class Relay
         WRITE_ERROR_UNKNOWN, DESTINATION_UNREACHABLE};
     private ErrorState errorState = ErrorState.NONE;
     private RelayChannel errorChannel; // The channel that errored.
+    private final int uniqueId;
+
+    private static int nextUniqueId;
+
+    public static final int ERROR_SOFT = 1;
+    public static final int ERROR_HARD = 2;
+    public static final int ERROR_NONE = 0;
 
     private class RelayChannel
     {
@@ -73,7 +80,7 @@ class Relay
                 || spec.getChanBLocalIP() == null && spec.getChanBRemoteIP() == null) {
             throw new IllegalArgumentException("At least one end of the link must be well known");
         }
-
+        uniqueId = getNextUniqueId();
     }
 
     /*
@@ -276,5 +283,11 @@ class Relay
     public boolean softError() {
         // Only one known type of soft error.
         return errorState != ErrorState.DESTINATION_UNREACHABLE;
+    }
+    public int getUniqueId() {
+        return uniqueId;
+    }
+    public static synchronized int getNextUniqueId() {
+        return nextUniqueId++;
     }
 }

app/src/main/java/com/daftdroid/simpleapps/udprelay/RelayButton.java
@@ -1,6 +1,7 @@
 package com.daftdroid.simpleapps.udprelay;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Handler;
 import android.support.v7.widget.AppCompatButton;
 
@@ -34,25 +35,29 @@ public class RelayButton extends AppCompatButton {
                 NetworkService.uiAddRelay(context, relay);
 
                 setText(spec.getName() + " [RUNNING]");
+                setTextColor(Color.BLACK);
             } else {
                 relay.stopRelay();
                 NetworkService.wakeup();
                 relay = null;
                 setText(spec.getName());
+                setTextColor(Color.BLACK);
             }
         } catch (IOException e) {
             setText(e.getLocalizedMessage()); // TODO
         }
     }
 
-    public void errorCallback(int severity, final String msg) {
-        post(new Runnable() {
-            @Override
-            public void run() {
-                setText(spec.getName() + " [" + msg + "]");
-            }
+    public void setError(int severity, final String msg) {
+        setText(spec.getName() + " [" + msg + "]");
 
-        });
+        if (severity == Relay.ERROR_HARD) {
+            setTextColor(Color.RED);
+        } else if (severity == Relay.ERROR_SOFT) {
+            setTextColor(Color.YELLOW);
+        } else {
+            setTextColor(Color.BLACK);
+        }
     }
 
     public Relay getRelay() {

commit ec2ff5770fea544535098a741d10a1785be71e70

Fix (I think) async logic of checking for new relays, stopping relays and errored relays. Likely a regression from 183b0828c9b6d0c32cfb2946b99c119d4fd6b6da

app/src/main/java/com/daftdroid/simpleapps/udprelay/NetworkService.java
@@ -149,8 +149,10 @@ public class NetworkService extends IntentService {
 
     private boolean processRegisteredRelays() {
         List<Relay> newRelaysCpy = null;
+        final boolean changed_ret;
 
         synchronized(NetworkService.class) {
+
             if (changed) {
                 // See if any new relays have appeared
 
@@ -171,9 +173,6 @@ public class NetworkService extends IntentService {
                     }
                 }
                 changed = false;
-
-        } else {
-                return false; // syncrhonized confirmation there is no more to do
             }
         }
 
@@ -198,18 +197,17 @@ public class NetworkService extends IntentService {
             }
         }
 
-        // If there are no relays, we can quit
+        // If all the relays are gone, and the UI hasn't asyncronously requested some new ones
+        // (by setting "changed"), then we can quit, indicated by setting singleton null.
 
         synchronized (NetworkService.class) {
-            if (registeredRelays.size() == 0) {
+            if (registeredRelays.size() == 0 && !changed) {
                 singleton = null; // tell async threads
                 finishing = true; // tell the caller
                 registeredRelays = null;
-                return false;
             }
+            return changed; // return the value of changed while we were syncrhonized
         }
-
-        return true;
     }
     public static synchronized  void uiAddRelay(Context ui, Relay r) {
 