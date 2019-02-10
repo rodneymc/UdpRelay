7aa43f5d6e8eb991761592c493d331f607a23877
rodney <rodney@daftdroid.com>
2019-02-10 12:46:28 +0000

    **** API CHANGE ****
    RelaySpec - main constructor now takes the parameters in a sane order.
    Removed the "reduced parameter" constructors, these are assymetric which is not in the spirit of this class - any client/server or otherwise asysymetric relationship should not be inferred at this level, especially with UDP (with the excpetion of example configs).

--- app/src/main/java/com/daftdroid/android/udprelay/RelaySpec.java
+++ app/src/main/java/com/daftdroid/android/udprelay/RelaySpec.java
@@ -7,7 +7,7 @@ import java.util.Objects;
 public class RelaySpec {
     public static final String EPHEMERAL_IP = null;
     public static final int EPHEMERAL_PORT = 0;
-    public static final int WELL_KNOWN_PORT = 1194; // Assuming OpenVPN
+    public static final int WELL_KNOWN_PORT_OPENVPN = 1194;
 
     private final String name;
     private final String chanAlocalIP;
@@ -35,9 +35,9 @@ public class RelaySpec {
         relay
      */
     public RelaySpec(String name, String chanAlocalIP, int chanAlocalPort,
+                     String chanAremoteIP, int chanAremotePort,
                      String chanBlocalIP, int chanBlocalPort,
-                     String chanBremoteIP, int chanBremotePort,
-                     String chanAremoteIP, int chanAremotePort) {
+                     String chanBremoteIP, int chanBremotePort) {
         this.name = name;
         this.chanAlocalIP = chanAlocalIP;
         this.chanAlocalPort = chanAlocalPort;
@@ -49,32 +49,6 @@ public class RelaySpec {
         this.chanAremotePort = chanAremotePort;
     }
 
-    /*
-        Constructor which assumes ephermal IP and port for outgoing traffic. UDP is connection-less
-        so this implies there is a client-server relationship established within the higher
-        levels of the prototcol. For OpenVPN over UDP, this relationship is established by the use
-        of TLS (tls-server / tls-client in the config files). Non-TLS UDP connections are peer-to-peer
-        and do not support multiple "clients".
-     */
-    public RelaySpec(String name, String chanAlocalIP, int chanAlocalPort,
-                     String chanBremoteIP, int chanBremotePort)
-    {
-        this (name, chanAlocalIP, chanAlocalPort,
-                EPHEMERAL_IP, EPHEMERAL_PORT,
-                chanBremoteIP, chanBremotePort,
-                EPHEMERAL_IP, EPHEMERAL_PORT);
-    }
-
-    /*
-        Constructor assuming a client-server type set-up for OpenVPN (see above comments
-        regarding TLS) - and both the server and the local replication of the server
-        will listen on a well known port for OpenVPN
-     */
-    public RelaySpec (String name, String chanAlocalIP, String chanBremoteIP)
-    {
-        this (name, chanAlocalIP, WELL_KNOWN_PORT,
-                chanBremoteIP, WELL_KNOWN_PORT);
-    }
 
     /*
         Examples - you can hard-code some settings here in your private branch
@@ -82,21 +56,15 @@ public class RelaySpec {
      */
 
     public static final RelaySpec[] exampleRelays=  {
-        // Example using the simplist constructor
-        new RelaySpec("demo 1", "192.168.42.129", "203.0.113.10"),
-
-        // Example where we must specify the dest ports
-        new RelaySpec("demo 2", "192.168.42.129",
-                1195, "203.0.113.10", 1196),
 
         // Example where we specify everything, note that the parameter chanBlocalIP is
         // very unlikely to be anything other than RelaySpec.EPHEMERAL_IP, unless you are
         // specifying a particular gateway - if so you need to know its IP at runtime.
 
-        new RelaySpec("demo 3", "192.168.42.129", 1195,
-                RelaySpec.EPHEMERAL_IP, 7000,
-                "203.0.113.10", 1196,
-                "192.168.42.150", 7000)
+        new RelaySpec("demo", "192.168.42.129", 1195, // our LAN addr
+                "192.168.42.150", 7000, // "client" on the LAN
+                 RelaySpec.EPHEMERAL_IP, 7000, // our GSM address
+                "203.0.113.10", 1196) // remote "server"
     };
 
     @Override
